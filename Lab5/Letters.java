771b99b0203b623dc6fa7b54f10583eb321a712a
Julia-Orlova <66700142+Julia-Orlova@users.noreply.github.com>
2023-06-22 11:44:45 +0300

    Create Letters.java

---
+++ Lab5/Letters.java
@@ -0,0 +1,9 @@
+package Lab5;
+
+public class Letters {
+    private char symbol;
+    public Letters(char symbol) { this.symbol = symbol;}
+    public String toString() {
+        return "" + symbol;
+    }
+}
